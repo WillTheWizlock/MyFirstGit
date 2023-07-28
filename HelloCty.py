8d4baf54a7cbc9052f9efb40471652ef9a887f36
WillSchl <WillTheWizlock@outlook.com>
2023-07-28 14:25:37 -0400

    added RNG

--- HelloCty.py
+++ HelloCty.py
@@ -1,4 +1,10 @@
-for c in "Hello CTY! ":
-    print(c);
-for i in range(1, 11):
-        print(i);
+import random
+
+for c in "Hello CTY!":
+    print(c)
+
+for i in range(1,11):
+    print(i)
+
+for i in range(1,11):
+    print(random.randint(2,8))
